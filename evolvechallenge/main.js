1bd8d69291f2d06a7c50e1b553a7562ffd0e2e54
rochydevs <rartiga3@gmail.com>
2017-03-10 15:20:04 +0100

    db

--- evolvechallenge/main.js
+++ evolvechallenge/main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import RequestView from './views/RequestView.js';
+//import DBConnection from './models/DBConnection.js';
 
 ReactDOM.render(<RequestView />, document.getElementById('container'));
 
