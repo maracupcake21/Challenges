bdc977add7bb1ddb9ece5c7a83a06310d1689324
rochydevs <rartiga3@gmail.com>
2017-03-14 11:30:37 +0100

    constants updated

--- evolvechallenge/helpers/const.js
+++ evolvechallenge/helpers/const.js
@@ -1,5 +1,12 @@
 module.exports = {
+    //Website
     websitePort: 3333,
+    //Service rest
+    serviceRest: "http://localhost:",
     serviceRestPort: 3000,
+    serviceRestMethod: "/PerformanceTest",
+    serviceRestUrl: "http://localhost:3000/",
+    serviceRestFullUrl: "http://localhost:3000/PerformanceTest",
+    //Database
     databaseConnectionString: "mongodb://localhost:27017/evolveChallenge"
 }
